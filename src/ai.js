b3e09c223db7916b67616b37fc59c52a515b01b4
OptimusCrimeNM <amin.xyu@gmail.com>
2025-03-16 20:35:34 +0200

    changed ai interaction

--- src/ai.js
+++ src/ai.js
@@ -7,7 +7,7 @@ export async function processWithAi(env, chatHistory, replyToChat) {
   context += 'Your username is "trainer". You are also known as "Sixth player", "Sixth" or @Sixth_Teammate_Bot.\n'
   context += 'You have recent chat history:\n' + chatHistory
   context += '\n\nPlease, respond to the chat if you want to reply or add anything.\n';
-  context += 'Do NOT start your message with technical info about message, it will be added by the chat system.\n';
+  context += 'Start your reply with "trainer writes..." or "trainer replies...", then message from a new line.\n';
   context += 'If you dont reply, write one word SKIP';
 
   const geminiPayload = { contents: [{ parts: [{ text: context }] }] };
@@ -20,8 +20,9 @@ export async function processWithAi(env, chatHistory, replyToChat) {
   if (!geminiResponse.ok) throw new Error(`Gemini API error: ${geminiResponse.status}`);
   const geminiData = await geminiResponse.json();
   const geminiReplyText = geminiData.candidates[0].content.parts[0].text;
-  if (geminiReplyText.startsWith("SKIP")) {
+  const lines = geminiReplyText.split('\n');
+  if (lines.length === 0 || lines[0].includes("SKIP")) {
     return new Response('Skipped with no response', { status: 200 });
   }
-  return replyToChat(geminiReplyText, true);
+  return replyToChat(lines.splice(0, 1).join('\n'), true);
 }
