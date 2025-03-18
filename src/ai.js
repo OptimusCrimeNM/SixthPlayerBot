ba0ccf696920b382b16bc20ec525f5dd6ba7eb90
OptimusCrimeNM <80789881+OptimusCrimeNM@users.noreply.github.com>
2025-03-18 14:41:29 +0200

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -55,7 +55,7 @@ export async function processWithAi(env, chatId, replyToChat) {
             const replyObject = JSON.parse(lines.join('\n').trim())
             if (replyObject.remove_note) await removeMemoryEntries(env, chatId, replyObject.remove_note);
             if (replyObject.add_note) await addMemoryEntries(env, chatId, replyObject.add_note);
-            if (replyObject.message && !replyObject.message_type == "skip") {
+            if (replyObject.message && replyObject.message_type != "skip") {
                 if (replyObject.message_desire_level > 90) return replyToChat(replyObject.message, true, true);
                 else if (replyObject.message_desire_level > 70) return replyToChat(replyObject.message, true, false);
             }
