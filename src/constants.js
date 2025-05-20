4bfc71a14e5e97e243fa760ac79a10147095e399
OptimusCrimeNM <80789881+OptimusCrimeNM@users.noreply.github.com>
2025-05-20 11:57:36 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 export const TELEGRAM_API_BASE_URL = 'https://api.telegram.org/bot';
-export const MAX_CHAT_HISTORY_CHARS = 32768;
+export const MAX_CHAT_HISTORY_CHARS = 8192;
 export const UNAUTHORIZED_MESSAGE = 'Ты кто такой чтобы это делать?! Давай до свидания!';
